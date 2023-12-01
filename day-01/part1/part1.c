0440286e8722ee258098b687fd6c409c71ecf5b1
Lemintex <aaron_nobile@hotmail.co.uk>
2023-12-01 23:47:39 +0000

    added day 1 part 1

---
+++ day-01/part1/part1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int processLine(char* line)
+{
+    int start = 0;
+    int end = 0;
+
+    int found = 0;
+    for (int i = 0; line[i] != '\0'; i++)
+    {
+        if (line[i] >= '0' && line[i] <= '9')
+        {
+            if (found == 0)
+            {
+                start = line[i] - '0';
+                found = 1;
+            }
+            end = line[i] - '0';
+        }
+    }
+    return start * 10 + end;
+}
+
+int main()
+{
+FILE* input = fopen("../input.txt", "r");
+int sum = 0;
+	char line[256];
+	while (fgets(line, sizeof(line), input))
+	{
+        sum += processLine(line);
+    }
+    printf("%d\n", sum);
+}
