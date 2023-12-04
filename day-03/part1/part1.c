930ef27d0c367aeac0bf74c7b9e95113f9330dd2
Lemintex <aaron_nobile@hotmail.co.uk>
2023-12-04 20:50:27 +0000

    STILL stuck on day 03 part 1

--- day-03/part1/part1.c
+++ day-03/part1/part1.c
@@ -3,16 +3,23 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define MAX 2048
 #define MAP_SIZE 140
 
+typedef struct {
+    int x;
+    int y;
+} symbol_t;
+
 char map[MAP_SIZE][MAP_SIZE] = {0};
-bool visited[MAP_SIZE][MAP_SIZE] = {false};
+symbol_t symbols[MAX] = {0};
 
 bool IsTouchingSymbol(int x, int y)
 {
     for (int xIndex = x - 1; xIndex <= x + 1; xIndex++) {
         for (int yIndex = y - 1; yIndex <= y + 1; yIndex++) {
             if (xIndex == x && yIndex == y) continue;
+            if (xIndex < 0 || xIndex >= MAP_SIZE || yIndex < 0 || yIndex >= MAP_SIZE) continue;
             char c = map[yIndex][xIndex];
             if (c != '.' && !(c >= '0' && c <= '9')) {
                 return true;
@@ -24,19 +31,27 @@ bool IsTouchingSymbol(int x, int y)
 void CheckMap()
 {
     int total = 0;
-    for (int y = 0; y < MAP_SIZE; y++) {
-        for (int x = 0; x < MAP_SIZE; x++) {
-            char* firstDigit = strpbrk(&map[y][x], "0123456789");
-            if (firstDigit >= &map[MAP_SIZE * y + 1][0] || firstDigit == NULL) break;
-            char* lastDigit = firstDigit + strspn(firstDigit, "0123456789");
-            if (lastDigit > &map[MAP_SIZE * y + 1][0]) lastDigit = map[MAP_SIZE * y + 1];
+    for (int y = 0; y < MAP_SIZE; y++)
+    {
+        char* pos = map[y];
+        while (true)
+        {
+            char* firstDigit = strpbrk(pos, "0123456789");
+            if (firstDigit >= map[MAP_SIZE * y + 1] || firstDigit == NULL)
+            {
+                break;
+            }
 
-            char number[10];
+            char* lastDigit = firstDigit + strspn(firstDigit, "0123456789");
+
+            char number[10] = {0};
             strncpy(number, firstDigit, lastDigit - firstDigit);
             int n = atoi(number);
+            pos = lastDigit;
 
             if (n == 0) continue;
-            bool isTouchingSymbol;
+            int x = firstDigit - map[y];
+            bool isTouchingSymbol = false;
             for (int posX = x; posX < x + (lastDigit - firstDigit); posX++) {
                 isTouchingSymbol |= IsTouchingSymbol(posX, y);
             }
@@ -44,9 +59,13 @@ void CheckMap()
             {
                 total += n;
             }
+            else
+            {
+                //printf("Symbol %d is not touching anything\n", n);
+            }
         }
     }
-    printf("%d", total);
+    printf("Total: %d", total);
 }
 
 int main()
