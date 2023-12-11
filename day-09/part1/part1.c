8300327cd064ea81893b73db5f6e83e7d1fac3f3
Lemintex <aaron_nobile@hotmail.co.uk>
2023-12-11 23:04:50 +0000

    progress on day 9 part 1

--- day-09/part1/part1.c
+++ day-09/part1/part1.c
@@ -1,7 +1,48 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-int sequence[200][20];
+#define SEQUENCE_LEN 20
+
+int sequence[200][SEQUENCE_LEN];
+int total = 0;
+int previous = 0;
+
+void CalculatePrediction(int n, int* dif)
+{
+
+    int* differences = (int*)malloc(sizeof(int) * (n+1)); // +1 for 'result' at the end
+    printf("%d: ", n);
+    bool isAllZero = true;
+    for (int i = 0; i < n-1; i++)
+    {
+        differences[i] = dif[i+1] - dif[i];
+        if (differences[i] != 0)
+        {
+            isAllZero = false;
+        }
+        printf("%d ", dif[i]);
+    }
+    if (isAllZero) 
+    {
+        free(differences);
+        return;
+    }
+    printf("\n");
+    if (n > 0)
+    {
+        CalculatePrediction(n - 1, differences);
+    }
+    previous = dif[n-1] + previous;
+    free(differences);
+    if (n == SEQUENCE_LEN)
+    {
+        //printf("result: %d\n", previous);
+        total += previous;
+    }
+}
+
 int main()
 {
     FILE* input = fopen("../input.txt", "r");
@@ -9,7 +50,6 @@ int main()
     // big char array to hold the line
     char line[256];
 
-int n = 0;
     for (int n = 0; fgets(line, sizeof(line), input); n++)
     {
         int index = 0;
@@ -36,11 +76,16 @@ int n = 0;
             }
         }
         /* code */
-        for (int i = 0; i < 20; i++)
-        {
-            printf("%d ", sequence[n][i]);
-        }
-        printf("\n");
+        //for (int i = 0; i < SEQUENCE_LEN; i++)
+        //{
+        //    printf("%d ", sequence[n][i]);
+        //}
+        //printf("\n");
     }
     
+    for (int i = 0; i < 200; i++)
+    {
+        CalculatePrediction(SEQUENCE_LEN, sequence[i]);
+    }
+    printf("Total: %d\n", total);
 }
