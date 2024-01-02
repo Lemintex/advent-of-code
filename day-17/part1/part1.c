ad60dac2dee9035a9e4e7207d145c29f8c302c3b
Lemintex <aaron_nobile@hotmail.co.uk>
2024-01-02 22:26:20 +0000

    Progress on day 17 part 1

--- day-17/part1/part1.c
+++ day-17/part1/part1.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdbool.h>
 
+int numberOfTilesInSameDirection = 0;
 typedef enum
 {
     NONE,
@@ -26,10 +27,91 @@ node_t** visitedNodes;
 node_t** map;
 int mapWidth = 0, mapHeight = 0;
 
-void FindShortestPath()
-{
+// PRIORITY QUEUE
+// ---------------------------------------------
+int size = 0;
 
+void swap(node_t* a, node_t* b)
+{
+    node_t temp = *b;
+    *b = *a;
+    *a = temp;
 }
+
+
+// heapify the tree
+void heapify(int array[], int size, int i)
+{
+    if (size == 1)
+    {
+        printf("Single element in the heap");
+    }
+    else
+    {
+        // Find the largest among root, left child and right child
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < size && array[l] > array[largest])
+        {
+            largest = l;
+        }
+        if (r < size && array[r] > array[largest])
+        {
+            largest = r;
+        }
+  
+        // Swap and continue heapifying if root is not largest
+        if (largest != i)
+        {
+          swap(&array[i], &array[largest]);
+          heapify(array, size, largest);
+        }
+    }
+}
+
+
+// insert an element into the tree
+void insert(int array[], int newNum)
+{
+    if (size == 0)
+    {
+        array[0] = newNum;
+        size += 1;
+    }
+    else
+    {
+        array[size] = newNum;
+        size += 1;
+        for (int i = size / 2 - 1; i >= 0; i--)
+        {
+          heapify(array, size, i);
+        }
+    }
+}
+
+// delete an element from the tree
+void deleteRoot(int array[], int num)
+{
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        if (num == array[i])
+        {
+            break;
+        }
+    }
+
+    swap(&array[i], &array[size - 1]);
+    size -= 1;
+    for (int i = size / 2 - 1; i >= 0; i--)
+    {
+        heapify(array, size, i);
+    }
+}
+
+// ---------------------------------------------
+// END PRIORITY QUEUE
 
 int main()
 {
@@ -53,7 +135,7 @@ int main()
         {
             node_t* node = &map[i][j];
             node->coolingValue = line[j] - '0';
-            node->pathValue = 999999;
+            node->pathValue = 99999999;
             node->visited = false;
             node->direction = NONE;
         }
