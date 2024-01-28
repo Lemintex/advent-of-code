9a7867ec2f63aab7b2470bb77dc3b6b92d4646ae
Lemintex <aaron_nobile@hotmail.co.uk>
2024-01-28 18:57:44 +0000

    Finished day 18 part 2

--- day-18/part2/part2.c
+++ day-18/part2/part2.c
@@ -15,7 +15,6 @@ point_t points[LENGTH] = {0};
 int main()
 {
     int minX = 0, minY = 0;
-    int maxX = 0, maxY = 0;
     int x = 0, y = 0;
 
     FILE* input = fopen("../input.txt", "r");
@@ -27,106 +26,60 @@ int main()
     while (fgets(line, sizeof(line), input))
     {
         int length = 0;
-        // test
-        length += atoi(&line[2]);
-        perimeter += length;
-        switch (line[0])
+        char* c = strchr(line, '#');
+        c++;
+        for (int i = 0; i < 5; i++)
         {
-            case 'R': // right
+            int num = 0;
+            if (c[i] >= '0' && c[i] <= '9')
+            {
+                num = c[i] - '0';
+            }
+            else if (c[i] >= 'a' && c[i] <= 'f')
+            {
+                num = c[i] - 'a' + 10;
+            }
+            length *= 16;
+            length += num; 
+        }
+        perimeter += length;
+        switch (c[5])
+        {
+            case '0': // right
                 x += length;
                 break;
-
-            case 'L': // left
+            case '2': // left
                 x -= length;
                 break;
-
-            case 'U': // up
+            case '3': // up
                 y -= length;
                 break;
-
-            case 'D': // down
+            case '1': // down
                 y += length;
                 break;
         }
         points[pointIndex++] = (point_t){x, y};
-        // char* c = strchr(line, '#');
-        // c++;
-        // for (int i = 0; i < 5; i++)
-        // {
-        //     int num = 0;
-        //     if (c[i] >= '0' && c[i] <= '9')
-        //     {
-        //         num = c[i] - '0';
-        //     }
-        //     else if (c[i] >= 'a' && c[i] <= 'f')
-        //     {
-        //         num = c[i] - 'a' + 10;
-        //     }
-        //     length *= 16;
-        //     length += num; 
-        //     printf("%c", c[i]);
-        // }
-        // printf(" %c", c[5]);
-        // switch (c[5])
-        // {
-        //     case '0': // right
-        //         x += length;
-        //         break;
 
-        //     case '2': // left
-        //         x -= length;
-        //         break;
-
-        //     case '3': // up
-        //         y -= length;
-        //         break;
-
-        //     case '1': // down
-        //         y += length;
-        //         break;
-        // }
-        // points[pointIndex++] = (point_t){x, y};
-
-        // printf(" %d\n", length);
-        // printf("\n");
         if (x < minX) minX = x;
         if (y < minY) minY = y;
-        if (x > maxX) maxX = x;
-        if (y > maxY) maxY = y;
     }
 
     // we need to ensure that all points are positive for gausses area formula
-    // this is done by subtracting the min x and y from all points
     for (int i = 0; i < LENGTH; i++)
     {
         
-        points[i].x -= minX;
-        //points[i].x += 1000; // add 1000 to ensure no negative numbers
-        points[i].y -= minY;
-        //points[i].y += 1000; // add 1000 to ensure no negative numbers
+        points[i].x += abs(minX);
+        points[i].y += abs(minY);
     }
 
     long int area = 0;
-    for (int i = 0; i < LENGTH - 1; i++)
-    {
-        area += points[i].x * points[i + 1].y - points[i + 1].x * points[i].y;
-    }
-    long int temp = points[LENGTH - 1].x * points[0].y - points[0].x * points[LENGTH - 1].y;
-    area += points[LENGTH - 1].x * points[0].y - points[0].x * points[LENGTH - 1].y;
-    area = labs(area) / 2;
-    printf("Clockwise area: %ld\n", area);
-
-    area = 0;
     for (int i = 0; i < LENGTH; i++)
     {
-        area -= points[i].x * points[(i+1)%LENGTH].y;
-        area += points[i].y * points[(i+1)%LENGTH].x;
+        area -= points[i].x * points[(i + 1) % LENGTH].y;
+        area += points[i].y * points[(i + 1) % LENGTH].x;
     }
     area = labs(area) / 2;
-    printf("Perimeter: %ld\n", perimeter);
     long int interior = area - perimeter / 2 + 1;
-    printf("Counter clockwise area: %ld\n", interior);
     printf("Total area: %ld\n", interior + perimeter);
     return 0;
 }
-// 1048840725 is too low
