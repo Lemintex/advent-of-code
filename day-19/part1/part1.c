2b015c0b4c9c8ab771744d4f824dd56058321651
Lemintex <aaron_nobile@hotmail.co.uk>
2024-01-31 00:31:12 +0000

    Parsing input for day 19

--- day-19/part1/part1.c
+++ day-19/part1/part1.c
@@ -3,6 +3,23 @@
 #include <string.h>
 #include <stdbool.h>
 
+typedef struct rule
+{
+    char* workflowName;
+    char type; // x, m, a, s
+    char operator; // <, >
+    int value;
+    char* targetWorkflowName;
+} rule_t;
+typedef struct workflow
+{
+    // store up to 4 rules
+    char* name;
+    rule_t rules[4];
+    int ruleCount;
+} workflow_t;
+
+
 int main()
 {
     FILE* input = fopen("../input.txt", "r");
@@ -22,7 +39,24 @@ int main()
                 readingInstructions = false;
                 continue;
             }
+            // read instruction
             instructionCount++;
+            char* name = strtok(line, "{");
+            workflow_t workflow;
+            workflow.name = name;
+            workflow.ruleCount = 0;
+
+            // get each rule
+            while(true)
+            {
+               char* rule = strtok(NULL, ","); 
+               printf("Rule: %s\n", rule);
+               if (rule == NULL)
+               {
+                   break;
+               }
+            }
+            printf("%s\n", name);
         }
         else
         {
