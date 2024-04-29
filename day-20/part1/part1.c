05d3a8d4af3d52dc4a9e96ed60c5f57b78f80fb6
Lemintex <aaron_nobile@hotmail.co.uk>
2024-04-30 00:17:44 +0100

    Fix day 20 parsing

--- day-20/part1/part1.c
+++ day-20/part1/part1.c
@@ -13,15 +13,29 @@ typedef struct module {
   char name[2];
 } module_t;
 
+module_t *modules;
+int module_count;
+
+int broadcaster_index = 0;
 int main() {
   FILE *input = fopen("../input.txt", "r");
 
   // big char array to hold the line
   char line[256];
 
+  int lines = 0;
   while (fgets(line, sizeof(line), input)) {
-    char *moudle_name = strtok(line, " ");
-    char *module_type = strtok(NULL, " ");
+    lines++;
+  }
+  modules = malloc(sizeof(module_t) * lines);
+  rewind(input);
+  for (int i = 0; fgets(line, sizeof(line), input); i++) {
+    char *module_name = strtok(line, " ");
+    if (strcmp(module_name, "broadcaster") == 0) {
+      broadcaster_index = i;
+      printf("Broadcaster index: %d\n", broadcaster_index);
+    }
+    char *unused = strtok(NULL, " ");
 
     char *tok[16];
     char *token = strtok(NULL, ", ");
@@ -30,9 +44,25 @@ int main() {
       tok[n++] = token;
       token = strtok(NULL, ", ");
     }
-    printf("%s %s\n", moudle_name, module_type);
-    for (int i = 0; i < n; i++) {
-      printf("%s\n", tok[i]);
+    modules[i].modules = malloc(sizeof(char *) * n);
+    for (int j = 0; j < n; j++) {
+      modules[i].modules[j] = malloc(sizeof(char) * 2);
+      strcpy(modules[i].modules[j], tok[j]);
     }
+    strcpy(modules[i].name, module_name);
+    if (module_name[0] == '%') {
+      modules[i].type = FLIPFLOP;
+    } else if (module_name[0] == '&') {
+      modules[i].type = CONJUNCTION;
+    } else if (module_name[0] == 'b') {
+      modules[i].type = BUTTON;
+    } else {
+      modules[i].type = RELAY;
+    }
+    printf("Module %s, type %d, modules: ", modules[i].name, modules[i].type);
+    for (int j = 0; j < n; j++) {
+      printf("%s ", modules[i].modules[j]);
+    }
+    printf("\n");
   }
 }
