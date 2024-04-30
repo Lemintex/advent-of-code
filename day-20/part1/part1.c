3f7c4c2e8e3ab0f5f5e955fb87dd6f62235e4eac
Lemintex <aaron_nobile@hotmail.co.uk>
2024-04-30 01:15:20 +0100

    Thi won't work, need a queue

--- day-20/part1/part1.c
+++ day-20/part1/part1.c
@@ -2,21 +2,33 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/queue.h>
+
 int pulses[2] = {0, 0};
 
 typedef enum module_type { BUTTON, RELAY, FLIPFLOP, CONJUNCTION } module_type_e;
+typedef enum pulse { LOW, HIGH } pulse_e;
 
 typedef struct module {
-  char state;
+  pulse_e pulse;
   char **modules;
+  int module_count;
   char type;
   char name[2];
+  int all_high;
 } module_t;
 
+void press_button_full(int times);
+void press_button_once();
+void handle_signal_for_module(module_t *module, pulse_e pulse);
+module_t *get_module_by_name(char *name);
 module_t *modules;
-int module_count;
+int module_total = 0;
 
 int broadcaster_index = 0;
+
+// QUEUE STUFF
+SLIST_HEAD(slisthead, entry) head = SLIST_HEAD_INITIALIZER(head);
 int main() {
   FILE *input = fopen("../input.txt", "r");
 
@@ -44,6 +56,7 @@ int main() {
       tok[n++] = token;
       token = strtok(NULL, ", ");
     }
+    modules[i].module_count = n;
     modules[i].modules = malloc(sizeof(char *) * n);
     for (int j = 0; j < n; j++) {
       modules[i].modules[j] = malloc(sizeof(char) * 2);
@@ -59,10 +72,68 @@ int main() {
     } else {
       modules[i].type = RELAY;
     }
-    printf("Module %s, type %d, modules: ", modules[i].name, modules[i].type);
-    for (int j = 0; j < n; j++) {
-      printf("%s ", modules[i].modules[j]);
-    }
-    printf("\n");
   }
+  press_button_full(1000);
+  printf("Pulses: %d %d\n", pulses[LOW], pulses[HIGH]);
+  printf("Total pulses: %d\n", pulses[LOW] + pulses[HIGH]);
+}
+
+void press_button_full(int times) {
+  for (int i = 0; i < times; i++) {
+    press_button_once();
+  }
+}
+
+void press_button_once() {
+  handle_signal_for_module(&modules[broadcaster_index], LOW);
+}
+
+void handle_signal_for_module(module_t *module, pulse_e pulse) {
+  static int num = 1;
+  printf("Num: %d\n", num++);
+  printf("Module: %s\n", module->name);
+  if (module->type == FLIPFLOP) {
+    if (pulse == LOW) {
+      if (module->pulse == LOW) {
+        module->pulse = HIGH;
+      } else {
+        module->pulse = LOW;
+      }
+      for (int i = 0; i < module->module_count; i++) {
+        pulses[module->pulse]++;
+        module_t *m = get_module_by_name(module->modules[i]);
+        handle_signal_for_module(m, module->pulse);
+      }
+    }
+  } else if (module->type == CONJUNCTION) {
+    if (pulse == LOW) {
+      module->all_high = 0;
+    }
+    if (module->all_high) {
+      pulse = HIGH;
+    } else {
+      pulse = LOW;
+    }
+    for (int i = 0; i < module->module_count; i++) {
+      pulses[pulse]++;
+      module_t *m = get_module_by_name(module->modules[i]);
+      handle_signal_for_module(m, pulse);
+    }
+  } else if (module->type == BUTTON) {
+    printf("Button %s\n", module->name);
+    for (int i = 0; i < module->module_count; i++) {
+      pulses[pulse]++;
+      module_t *m = get_module_by_name(module->modules[i]);
+      handle_signal_for_module(m, pulse);
+    }
+  }
+}
+
+module_t *get_module_by_name(char *name) {
+  for (int i = 0; i < module_total; i++) {
+    if (strcmp(modules[i].name, name) == 0) {
+      return &modules[i];
+    }
+  }
+  return NULL;
 }
