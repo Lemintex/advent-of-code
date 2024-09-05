e1861aec5e8ffc4bd50898edf78563f9547a788f
Lemintex <aaron_nobile@hotmail.co.uk>
2024-09-05 23:55:07 +0100

    progress on this TAKING FOREVER OMG

--- day-20/part1/part1.c
+++ day-20/part1/part1.c
@@ -26,12 +26,15 @@ typedef struct module {
   module_memory_t memory;
 } module_t;
 
-
 typedef struct queue {
   int front, rear, size;
   unsigned capacity;
   int *array;
 } queue_t;
+
+void do_button_presses();
+void button_press(int *low, int *high);
+int is_in_module(char *mod);
 
 queue_t *create_queue(unsigned capacity) {
   queue_t *queue = (queue_t *)malloc(sizeof(queue_t));
@@ -57,8 +60,7 @@ void enqueue(queue_t *queue, int itemIndex) {
 
 int dequeue(queue_t *queue) {
   if (is_empty(queue)) {
-    module_t* empty;
-    return empty;
+    return -1;
   }
   int item = queue->array[queue->front];
   queue->front = (queue->front + 1) % queue->capacity;
@@ -67,6 +69,7 @@ int dequeue(queue_t *queue) {
 }
 int broadcast_index = 0;
 module_t *modules;
+int module_count;
 queue_t *queue;
 
 int main() {
@@ -84,7 +87,7 @@ int main() {
   rewind(input);
 
   modules = (module_t *)malloc(sizeof(module_t) * lines);
-  int module_count = lines;
+  module_count = lines;
   for (int i = 0; fgets(line, sizeof(line), input); i++) {
     modules[i].memory.input_count = 0;
     modules[i].memory.inputs = NULL;
@@ -164,7 +167,7 @@ int main() {
 
           // set the pulse to low
           modules[i].memory.inputs[modules[i].memory.input_count].pulse = LOW;
-          
+
           // increment the input count
           modules[i].memory.input_count++;
           break;
@@ -177,31 +180,73 @@ int main() {
     printf("Input count: %d\n", modules[i].memory.input_count);
     for (int j = 0; j < modules[i].memory.input_count; j++) {
       printf("Input %d: %s\n", j, modules[i].memory.inputs[j].name);
-    printf("Pulse: %d\n", modules[i].memory.inputs[j].pulse);
+      printf("Pulse: %d\n", modules[i].memory.inputs[j].pulse);
     }
     for (int j = 0; j < modules[i].target_count; j++) {
       printf("Target %d: %s\n", j, modules[i].targets[j]);
     }
   }
+  do_button_presses();
 }
 
 void do_button_presses() {
   int low = 0;
   int high = 0;
-  for (int i = 0; i < 1000; i++)
-  {
+  for (int i = 0; i < 1000; i++) {
     low++;
-
-    enqueue(queue, &(modules[broadcast_index]));
-    /* code */
+    enqueue(queue, broadcast_index);
+    button_press(&low, &high);
   }
-  
+  printf("%d\n", low);
+  printf("%d\n", high);
+  printf("%d\n", high * low);
 }
 
-void button_press() {
+void button_press(int *low, int *high) {
+  while (!is_empty(queue)) {
+    int i = dequeue(queue);
+    module_t module = modules[i];
+    if (module.memory.pulse == LOW) {
+      *low = *low + 1;
+    } else {
+      *high = *high + 1;
+    }
+    pulse_e output;
+    if (module.type == FLIPFLOP) {
+      if (module.memory.pulse == LOW) {
+        if (module.memory.pulse == LOW) {
+          module.memory.pulse = HIGH;
+        } else {
+          module.memory.pulse = LOW;
+        }
+      }
+      output = module.memory.pulse;
 
+      for (int i = 0; i < module.target_count; i++) {
+        modules[i].memory.pulse = module.memory.pulse;
+        enqueue(queue, i);
+      }
+    } else if (module.type == CONJUNCTION) {
+      output = HIGH;
+      for (int i = 0; module.memory.input_count; i++) {
+        if (module.memory.inputs[i].pulse == LOW) {
+          output = LOW;
+          break;
+        }
+      }
+      for (int i = 0; i < module.target_count; i++) {
+        int t = is_in_module(module.targets[i]);
+        modules[t].memory.pulse = output;
+      }
+    }
+  }
 }
 
-void send_pulse() {
-
+int is_in_module(char *mod) {
+  for (int i = 0; i < module_count; i++) {
+    if (strcmp(modules[i].name, mod)) {
+      return i;
+    }
+  }
+  return 0;
 }
