3e3367bcd293be30670f668c793373e16f15db2b
Lemintex <aaron_nobile@hotmail.co.uk>
2024-09-08 02:00:20 +0100

    Refactored memory to pass pointers

--- day-20/part1/part1.c
+++ day-20/part1/part1.c
@@ -32,7 +32,8 @@ typedef struct queue {
 
 void do_button_presses();
 void button_press(int *low, int *high);
-int is_in_module(char *mod);
+module_t *is_in_module(module_t *mod);
+module_t *is_in_module_str(char *str);
 void send_pulse_to_targets(module_t *mod, pulse_e pulse);
 void update_targets_memory_flipflop(module_t *mod);
 void update_targets_memory_conjunction(module_t *mod);
@@ -121,21 +122,21 @@ int main() {
   }
   rewind(input);
   for (int i = 0; fgets(line, sizeof(line), input); i++) {
+    if (i == 54)
+      continue;
     // set up the module targets
     printf("Mod %d: \n", i);
     char *target = strtok(rest[i], ",\n");
     while (target != NULL) {
-      printf("%s len %d, ", target, strlen(target));
+      // printf("%s len %d, ", target, strlen(target));
       for (int l = 0; l < strlen(target); l++) {
         printf("%d ", target[l]);
       }
       modules[i].target.targets = (module_t **)realloc(
           modules[i].target.targets,
           sizeof(module_t *) * modules[i].target.target_count + 1);
-      int target_index = is_in_module(target);
-      printf("%d, ", target_index);
-      modules[i].target.targets[modules[i].target.target_count] =
-          &modules[target_index];
+      module_t *t = is_in_module_str(target);
+      modules[i].target.targets[modules[i].target.target_count] = t;
       modules[i].target.target_count++;
       target = strtok(NULL, "\n,");
       if (target != NULL) {
@@ -151,130 +152,129 @@ int main() {
       printf("Target %d: %s\n", j, modules[i].target.targets[j]->name);
     }
   }
-  // // set up the module targets
-  // for (int i = 0; i < module_count; i++) {
-  //   modules[i].memory.inputs =
-  //       (input_memory_t *)malloc(sizeof(input_memory_t) * 0);
-  //   for (int j = 0; j < modules[i].target_count; j++) {
-  //     for (int k = 0; k < module_count; k++) {
-  //       if (i == k || modules[k].type == BROADCAST) {
-  //         continue;
-  //       }
-  //       if (strcmp(modules[i].targets[j], modules[k].name) == 0) {
-  //         // allocate memory for the new input
-  //         modules[i].memory.inputs = (input_memory_t *)realloc(
-  //             modules[i].memory.inputs,
-  //             sizeof(input_memory_t) * (modules[i].memory.input_count + 1));
-  //
-  //         // allocate memory for the name of the input
-  //         modules[i].memory.inputs[modules[i].memory.input_count].name =
-  //             (char *)malloc(sizeof(char) * strlen(modules[k].name));
-  //
-  //         // copy the name of the input
-  //         strcpy(modules[i].memory.inputs[modules[i].memory.input_count].name,
-  //                modules[k].name);
-  //
-  //         // set the pulse to low
-  //         modules[i].memory.inputs[modules[i].memory.input_count].pulse =
-  //         LOW;
-  //
-  //         // increment the input count
-  //         modules[i].memory.input_count++;
-  //         break;
-  //       }
-  //     }
-  //   }
-  // }
-  // for (int i = 0; i < module_count; i++) {
-  //   printf("Module %d: %s\n", i, modules[i].name);
-  //   printf("Input count: %d\n", modules[i].memory.input_count);
-  //   for (int j = 0; j < modules[i].memory.input_count; j++) {
-  //     printf("Input %d: %s\n", j, modules[i].memory.inputs[j].name);
-  //     printf("Pulse: %d\n", modules[i].memory.inputs[j].pulse);
-  //   }
-  //   for (int j = 0; j < modules[i].target_count; j++) {
-  //     printf("Target %d: %s\n", j, modules[i].targets[j]);
-  //   }
-  // }
-  // do_button_presses();
-}
-//
-// void do_button_presses() {
-//   int low = 0;
-//   int high = 0;
-//   for (int i = 0; i < 1; i++) {
-//     low++;
-//     button_press(&low, &high);
-//   }
-//   printf("%d\n", low);
-//   printf("%d\n", high);
-//   printf("%d\n", high * low);
-// }
-//
-// void button_press(int *low, int *high) {
-//   for (int n = 0; n < modules[broadcast_index].target_count; n++) {
-//     int m = is_in_module(modules[broadcast_index].targets[n]);
-//     if (m < 0) {
-//       continue;
-//     }
-//     enqueue(queue, m);
-//   }
-//   while (!is_empty(queue)) {
-//     int i = dequeue(queue);
-//     module_t module = modules[i];
-//     if (module.memory.pulse == LOW) {
-//       *low = *low + 1;
-//     } else {
-//       *high = *high + 1;
-//     }
-//     pulse_e output;
-//
-//     switch (module.type) {
-//     case FLIPFLOP:
-//       if (module.pulse == HIGH) {
-//         break;
-//       }
-//       // if this guard is passed we flip the output
-//       if (module.memory.pulse == LOW) {
-//         module.memory.pulse = HIGH;
-//       } else {
-//         module.memory.pulse = LOW;
-//       }
-//       output = module.memory.pulse;
-//
-//       // WRONG
-//       for (int i = 0; i < module.target_count; i++) {
-//         modules[i].memory.pulse = module.memory.pulse;
-//         modules[i].pulse = module.memory.pulse;
-//         enqueue(queue, i);
-//       }
-//
-//       break;
-//
-//     case CONJUNCTION:
-//       output = HIGH;
-//       for (int i = 0; module.memory.input_count; i++) {
-//         if (module.memory.inputs[i].pulse == LOW) {
-//           output = LOW;
-//           break;
-//         }
-//       }
-//
-//       module.pulse = output;
-//       update_targets_memory_conjunction(&module);
-//
-//       break;
-//     }
-//   }
-// }
-//
-int is_in_module(char *mod) {
+  // set up the module inputs
   for (int i = 0; i < module_count; i++) {
-    if (strcmp(modules[i].name, mod) == 0) {
-      return i;
+    printf("%d\n", i);
+    modules[i].input.inputs = (module_t **)malloc(0);
+    for (int j = 0; j < modules[i].target.target_count; j++) {
+      for (int k = 0; k < module_count; k++) {
+        if (i == k || modules[k].type == BROADCAST || i == 54) {
+          continue;
+        }
+        if (strcmp(modules[i].target.targets[j]->name, modules[k].name) == 0) {
+          // allocate memory for the new input
+          modules[i].input.inputs = (module_t **)realloc(
+              modules[i].input.inputs,
+              sizeof(module_t *) * (modules[i].input.input_count + 1));
+          modules[i].input.inputs[modules[i].input.input_count] = &modules[k];
+
+          // increment the input count
+          modules[i].input.input_count++;
+          break;
+        }
+      }
     }
   }
-  return -1;
+  for (int i = 0; i < module_count; i++) {
+    printf("Module %d: %s\n", i, modules[i].name);
+    printf("Input count: %d\n", modules[i].input.input_count);
+    for (int j = 0; j < modules[i].input.input_count; j++) {
+      printf("Input %d: %s\n", j, modules[i].input.inputs[j]->name);
+      printf("Pulse: %d\n", modules[i].input.inputs[j]->input.pulse);
+    }
+    for (int j = 0; j < modules[i].target.target_count; j++) {
+      printf("Target %d: %s\n", j, modules[i].target.targets[j]->name);
+    }
+  }
+  do_button_presses();
+}
+
+void do_button_presses() {
+  int low = 0;
+  int high = 0;
+  for (int i = 0; i < 1; i++) {
+    low++;
+    button_press(&low, &high);
+  }
+  printf("%d\n", low);
+  printf("%d\n", high);
+  printf("%d\n", high * low);
+}
+
+void button_press(int *low, int *high) {
+  for (int n = 0; n < modules[broadcast_index].target.target_count; n++) {
+    module_t *mod = is_in_module(modules[broadcast_index].target.targets[n]);
+    if (mod == NULL) {
+      continue;
+    }
+    enqueue(queue, mod);
+  }
+  while (!is_empty(queue)) {
+    module_t *module = dequeue(queue);
+    printf("Name: %s\nType:%d\nPulse:%d\n-------------\n", module->name, module->type, module->input.pulse);
+    if (module->input.pulse == LOW) {
+      *low = *low + 1;
+    } else {
+      *high = *high + 1;
+    }
+    pulse_e output;
+
+    switch (module->type) {
+    case FLIPFLOP:
+      if (module->input.pulse == HIGH) {
+        break;
+      }
+      // if this guard is passed we flip the output
+      if (module->input.pulse == LOW) {
+        module->input.pulse = HIGH;
+      } else {
+        module->input.pulse = LOW;
+      }
+
+      // WRONG
+      for (int i = 0; i < module->target.target_count; i++) {
+        modules[i].input.pulse = module->input.pulse;
+        enqueue(queue, &modules[i]);
+      }
+
+      break;
+
+    case CONJUNCTION:
+      output = HIGH;
+      for (int i = 0; module->input.input_count; i++) {
+        if (module->input.inputs[i]->input.pulse == LOW) {
+          output = LOW;
+          break;
+        }
+      }
+
+      module->input.pulse = output;
+      for (int i = 0; i < module->target.target_count; i++) {
+        modules[i].input.pulse = module->input.pulse;
+        enqueue(queue, &modules[i]);
+      }
+
+      break;
+    }
+  }
+}
+
+module_t *is_in_module(module_t *mod) {
+  for (int i = 0; i < module_count; i++) {
+    if (strcmp(modules[i].name, mod->name) == 0) {
+      return &modules[i];
+    }
+  }
+  return NULL;
+}
+
+module_t *is_in_module_str(char *str) {
+  for (int i = 0; i < module_count; i++) {
+    if (strcmp(modules[i].name, str) == 0) {
+      return &modules[i];
+    }
+  }
+  return NULL;
 }
 //
 // void update_targets_memory_flipflop(module_t *mod) {
