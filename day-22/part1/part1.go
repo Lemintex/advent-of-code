bbc472d36f665f454cceef25b586bceeba64fb90
Lemintex <aaron_nobile@hotmail.co.uk>
2024-09-17 23:07:06 +0100

    figuring approach for day part 1

--- day-22/part1/part1.go
+++ day-22/part1/part1.go
@@ -1,6 +1,5 @@
 package main
 
-
 import (
 	"fmt"
 	"os"
@@ -12,21 +11,33 @@ type vec3 struct {
 	x, y, z int
 }
 
+type plane struct {
+	v vec3
+	length, width int
+} 
+
 type brick struct {
-	start, end vec3
+	top, bottom plane
+	supporting []*brick
+	relying []*brick
 }
 
+var minX, minY, maxX, maxY, lenX, lenY int
 var bricks brick
 var brickMap []string
 func main() {
+	minX, minY = 1000000, 1000000
 	f, err := os.ReadFile("../input.txt")
 	if err != nil {
 		panic("err`=")
 	}
-	brickMap = strings.Split(string(f), "\n")
 	for _, l := range brickMap {
 		ends := strings.Split(l, "~")
-		fmt.Printf("\nStart: %v, End: %v", ends[0], ends[1])
+		// TODO: find proper fix
+		if len(ends) == 1 { // temporary
+			break
+		}
+		fmt.Println(ends)
 		b1, b2 := strings.Split(ends[0], ","), strings.Split(ends[1], ",")
 		v1X, _ := strconv.Atoi(b1[0])
 		v1Y, _ := strconv.Atoi(b1[1])
@@ -36,25 +47,48 @@ func main() {
 		v2Y, _ := strconv.Atoi(b2[1])
 		v2Z, _ := strconv.Atoi(b2[2])
 
+		minX = min(minX, v1X)
+		minY = min(minY, v1Y)
+		maxX = max(maxX, v2X)
+		maxY = max(maxY, v2Y)
 		v1 := vec3 {
 			x: v1X,
 			y: v1Y,
 			z: v1Z,
 		}
+		p1 := plane {
+			v: v1,
+			length: v2X - v1X + 1,
+			width: v2Y - v1Y + 1,
+		}
 		v2 := vec3 {
-			x: v2X,
-			y: v2Y,
+			x: v1X,
+			y: v1Y,
 			z: v2Z,
 		}
+		p2 := plane {
+			v: v2,
+			length: v2X - v1X + 1,
+			width: v2Y - v1Y + 1,
+		}
 		brick := brick {
-			start: v1,
-			end: v2,
+			bottom: p2,
+			top: p1,
+			supporting: []*brick{},
+			relying: []*brick{},
 		}
 		brick.PrintBrick()
 	}
+	lenX = maxX - minX + 1
+	lenY = maxY - minY + 1
+	fmt.Println(maxX, minX, lenX)
+	fmt.Println(maxY, minY, lenY)
 }
 
 func (b* brick) PrintBrick() {
-	fmt.Printf("\nStart = x: %d, y: %d, z: %d | ", b.start.x, b.start.y, b.start.z)
-	fmt.Printf("End = x: %d, y: %d, z: %d | ", b.end.x, b.end.y, b.end.z)
+	// fmt.Printf("\nStart = x: %d, y: %d, z: %d, Area: %d x %d | ", b.start.x, b.start.y, b.start.z, b.start.length, b.start.width)
+	// fmt.Printf("End = x: %d, y: %d, z: %d, Area: %d x %d", b.end.x, b.end.y, b.end.z, b.end.length, b.end.width)
 }
+
+//ooooo
+//o    <=
