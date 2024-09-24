15c601f38eaba0215cdf674cad91dc66602c24f8
Lemintex <aaron_nobile@hotmail.co.uk>
2024-09-24 22:25:14 +0100

    Building graph for day 23

--- day-23/part1/part1.go
+++ day-23/part1/part1.go
@@ -6,14 +6,47 @@ import (
 	"strings"
 )
 
+type node struct {
+	x, y  int
+	steps int
+	nodes []*node
+}
+
+var root node
+var nodes map[[2]int]*node
+var sx, sy int
 var trail []string
+
 func main() {
+	nodes = make(map[[2]int]*node)
 	f, err := os.ReadFile("../input.txt")
 	if err != nil {
 		panic("AAH")
 	}
 	trail = strings.Split(string(f), "\n")
-	for _, t := range trail {
-		fmt.Println(t)
+	sy = 0
+	for i, t := range trail {
+		if i == 0 {
+			sx = strings.Index(t, ".")
+		}
+	}
+	parseMapIntoGraph()
+}
+
+
+func parseMapIntoGraph() {
+	var stack [][2]int
+	begin := node {
+		x: sx,
+		y: sy,
+		steps: 0,
+	}
+	root = begin
+	stack = append(stack, [2]int{sx, sy})
+	nodes[[2]int{sx, sy}] = &root
+	for len(stack) > 0 {
+		s := stack[len(stack) - 1]
+		stack = stack[:max(0, len(stack) - 2)]
+		fmt.Println(s)
 	}
 }
