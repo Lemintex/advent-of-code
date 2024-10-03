806107e4c968f71ceca3993890fc6c82a1613b09
Lemintex <aaron_nobile@hotmail.co.uk>
2024-10-03 02:51:39 +0100

    finished day 24 part 1

--- day-24/part1/part1.go
+++ day-24/part1/part1.go
@@ -3,20 +3,26 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 )
 
-type vec3 struct {
-	x, y, z int
+var MIN, MAX float32
+
+type Vec3 struct {
+	X, Y, Z int
 }
 
-type hailstone struct {
-	pos, vel vec3
+type Hailstone struct {
+	pos, v  Vec3
+	a, b, c float32
 }
 
-var hailstones []hailstone
+var hailstones []Hailstone
+
 func main() {
+	MIN =  200000000000000
+	MAX =  400000000000000
 	f, err := os.ReadFile("../input.txt")
 	if err != nil {
 		panic(err)
@@ -25,6 +31,7 @@ func main() {
 	for _, s := range str {
 		parseHailstone(s)
 	}
+	CalculateIntersections()
 }
 
 func parseHailstone(s string) {
@@ -44,12 +51,12 @@ func parseHailstone(s string) {
 		panic(errz)
 	}
 
-	pos := vec3 {
-		x: x,
-		y: y,
-		z: z,
+	pos := Vec3{
+		X: x,
+		Y: y,
+		Z: z,
 	}
-	velocity := strings.Split(parts[2], ", ")
+	velocity := strings.Split(parts[1], ", ")
 	vx, errvx := strconv.Atoi(velocity[0])
 	if errvx != nil {
 		panic(errvx)
@@ -62,15 +69,50 @@ func parseHailstone(s string) {
 	if errvz != nil {
 		panic(errvz)
 	}
-	vel := vec3 {
-		x: vx,
-		y: vy,
-		z: vz,
+	vel := Vec3{
+		X: vx,
+		Y: vy,
+		Z: vz,
 	}
-	stone := hailstone {
+	a, b, c := float32(vy), float32(-vx), float32(vy*x-vx*y)
+	stone := Hailstone{
 		pos: pos,
-		vel: vel,
+		v:   vel,
+		a:   a,
+		b:   b,
+		c:   c,
 	}
 	hailstones = append(hailstones, stone)
-	fmt.Println(" ")
 }
+
+func CalculateIntersections() {
+	total := 0
+	for i, h1 := range hailstones {
+		for _, h2 := range hailstones[i+1:] {
+			if h1.Intersection(&h2) {
+				total++
+			}
+		}
+	}
+	fmt.Println(total)
+}
+
+func (h *Hailstone) Intersection(other *Hailstone) bool {
+	// if lines are parallel
+	if h.a*other.b == other.a*h.b {
+		return false
+	}
+
+	// get intersection x,y
+	x, y := (h.c*other.b-other.c*h.b)/(h.a*other.b-other.a*h.b), (other.c*h.a-h.c*other.a)/(h.a*other.b-other.a*h.b)
+
+	// if outwith bounding box
+	if x < MIN || x > MAX || y < MIN || y > MAX {
+		return false
+	}
+	//return if intersection point is after the starting point of both lines
+	return (x-float32(h.pos.X))*float32(h.v.X) >= 0 && (y-float32(h.pos.Y))*float32(h.v.Y) >= 0 && (x-float32(other.pos.X))*float32(other.v.X) >= 0 && (y-float32(other.pos.Y))*float32(other.v.Y) >= 0
+
+}
+//11631 too low
+//15982 too high
