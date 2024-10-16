58633a89ccf33d4800d3d531356c5d67c02af4b6
Lemintex <aaron_nobile@hotmail.co.uk>
2024-10-16 02:15:03 +0100

    improved day 3

--- day-03/part1/part1.c
+++ day-03/part1/part1.c
@@ -1,99 +1,94 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
 
 #define MAX 2048
 #define MAP_SIZE 140
 
 typedef struct {
-    int x;
-    int y;
+  int x;
+  int y;
 } symbol_t;
+
+bool is_touching_symbol(int x, int y);
+void check_map();
 
 char map[MAP_SIZE][MAP_SIZE] = {0};
 symbol_t symbols[MAX] = {0};
-    int stars = 0;
 
-bool IsTouchingSymbol(int x, int y)
-{
-    for (int xIndex = x - 1; xIndex <= x + 1; xIndex++) {
-        for (int yIndex = y - 1; yIndex <= y + 1; yIndex++) {
-            if (xIndex == x && yIndex == y) continue;
-            if (xIndex < 0 || xIndex >= MAP_SIZE || yIndex < 0 || yIndex >= MAP_SIZE) continue;
-            char c = map[yIndex][xIndex];
-            if (c == '*') {
-                stars++;
-                return true;
-            }
-        }
+bool is_touching_symbol(int x, int y) {
+  for (int x_index = x - 1; x_index <= x + 1; x_index++) {
+    for (int y_index = y - 1; y_index <= y + 1; y_index++) {
+      if (x_index == x && y_index == y)
+        continue;
+      if (x_index < 0 || x_index >= MAP_SIZE || y_index < 0 || y_index >= MAP_SIZE)
+        continue;
+      char c = map[y_index][x_index];
+      if (c != '.' && !(c >= '0' && c <= '9')) {
+        return true;
+      }
     }
-    return false;
-}
-void CheckMap()
-{
-    int total = 0;
-    for (int y = 0; y < MAP_SIZE; y++)
-    {
-        char* pos = map[y];
-        char* lastPos = map[y+1];
-        while (true)
-        {
-            char* firstDigit = strpbrk(pos, "0123456789");
-            if (firstDigit >= map[y + 1] || firstDigit == NULL)
-            {
-                break;
-            }
-
-            char* lastDigit = firstDigit + strspn(firstDigit, "0123456789");
-            int length = 0;
-            if (lastDigit >= map[y + 1] || lastDigit == NULL)
-            {
-                lastDigit = map[y + 1] - 1;
-                length = lastDigit - firstDigit;
-                length++;
-            }
-            else
-            {
-                length = lastDigit - firstDigit;
-            }
-
-            char number[10] = {0};
-            strncpy(number, firstDigit, length);
-            int n = atoi(number);
-            pos = lastDigit;
-
-            if (n == 0) continue;
-            int x = firstDigit - map[y];
-            bool isTouchingSymbol = false;
-            for (int posX = x; posX < x + length; posX++) {
-                isTouchingSymbol = IsTouchingSymbol(posX, y);
-                if (isTouchingSymbol) break;
-            }
-            if (isTouchingSymbol)
-            {
-                total += n;
-            }
-            if (lastDigit >= lastPos - 1)
-            {
-                break;
-            }
-        }
-    }
-    printf("Total: %d", stars);
+  }
+  return false;
 }
 
-int main()
-{
-    FILE* input = fopen("../input.txt", "r");
+void check_map() {
+  int total = 0;
+  for (int y = 0; y < MAP_SIZE; y++) {
+    char *pos = map[y];
+    char *last_pos = map[y + 1];
+    while (true) {
+      char *first_digit = strpbrk(pos, "0123456789");
+      if (first_digit >= map[y + 1] || first_digit == NULL) {
+        break;
+      }
 
-    char line[256];
-    
-    int y = 0;
-    for (int i = 0; fgets(line, 256, input) != NULL; i++) {
-        memcpy(&map[i], line, MAP_SIZE);
+      char *last_digit = first_digit + strspn(first_digit, "0123456789");
+      int length = 0;
+      if (last_digit >= map[y + 1] || last_digit == NULL) {
+        last_digit = map[y + 1] - 1;
+        length = last_digit - first_digit;
+        length++;
+      } else {
+        length = last_digit - first_digit;
+      }
+
+      char number[10] = {0};
+      strncpy(number, first_digit, length);
+      int n = atoi(number);
+      pos = last_digit;
+
+      if (n == 0)
+        continue;
+      int x = first_digit - map[y];
+      bool touching_symbol = false;
+      for (int pos_x = x; pos_x < x + length; pos_x++) {
+        touching_symbol = is_touching_symbol(pos_x, y);
+        if (touching_symbol)
+          break;
+      }
+      if (touching_symbol) {
+        total += n;
+      }
+      if (last_digit >= last_pos - 1) {
+        break;
+      }
     }
-    
-    CheckMap();
+  }
+  printf("Total: %d", total);
+}
+
+int main() {
+  FILE *input = fopen("../input.txt", "r");
+
+  char line[256];
+
+  int y = 0;
+  for (int i = 0; fgets(line, 256, input) != NULL; i++) {
+    memcpy(&map[i], line, MAP_SIZE);
+  }
+
+  check_map();
 }
--- day-03/part2/part2.c
+++ day-03/part2/part2.c
@@ -1,159 +1,153 @@
+#include <ctype.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
-#include <ctype.h>
 
 #define MAX 2048
 #define MAP_SIZE 140
-int gearX = 0, gearY = 0;
-//can't have 3 gears touching each other
+int gear_x = 0, gear_y = 0;
+
+// can't have 3 gears touching each other
 typedef struct {
-    int x;
-    int y;
-    int parts[2];
-    int gears;//rename
-    bool isValid;
+  int x;
+  int y;
+  int parts[2];
+  int gears; // rename
+  bool is_valid;
 } gears_t;
+
+void add_to_new_gear(int n);
+void add_to_existing_gear_info(int n, int index);
+void check_existing_gear_info(int n);
+bool is_touching_gear(int x, int y);
+void calculate_gear_ratio();
+void check_map();
 
 char map[MAP_SIZE][MAP_SIZE] = {0};
 gears_t gears[MAX];
-int gearsCount = -1;
+int gears_count = -1;
 
-void AddToNewGear(int n)
-{
-    gearsCount++;
-    gears[gearsCount].x = gearX;
-    gears[gearsCount].y = gearY;
-    gears[gearsCount].parts[0] = n;
-    gears[gearsCount].gears = 1;
-    gears[gearsCount].isValid = true;
+void add_to_new_gear(int n) {
+  gears_count++;
+  gears[gears_count].x = gear_x;
+  gears[gears_count].y = gear_y;
+  gears[gears_count].parts[0] = n;
+  gears[gears_count].gears = 1;
+  gears[gears_count].is_valid = true;
 }
 
-void AddToExistingGearInfo(int n, int index)
-{
-    gears[index].parts[1] = n;
-    gears[index].gears = 2;
+void add_to_existing_gear_info(int n, int index) {
+  gears[index].parts[1] = n;
+  gears[index].gears = 2;
 }
 
-void CheckExistingGearInfo(int n)
-{
-    for (int i = 0; i <= gearsCount; i++) {
-        if (gears[i].x == gearX && gears[i].y == gearY) {
-            if (gears[i].gears == 2 && gears[i].isValid)
-            {
-                gears[i].isValid = false;
-            }
-            else
-            {
-                AddToExistingGearInfo(n, i);
-            }
-            return;
+void check_existing_gear_info(int n) {
+  for (int i = 0; i <= gears_count; i++) {
+    if (gears[i].x == gear_x && gears[i].y == gear_y) {
+      if (gears[i].gears == 2 && gears[i].is_valid) {
+        gears[i].is_valid = false;
+      } else {
+        add_to_existing_gear_info(n, i);
+      }
+      return;
+    }
+  }
+  add_to_new_gear(n);
+}
+
+bool is_touching_gear(int x, int y) {
+  for (int x_index = x - 1; x_index <= x + 1; x_index++) {
+    for (int y_index = y - 1; y_index <= y + 1; y_index++) {
+      if (x_index == x && y_index == y)
+        continue;
+      if (x_index < 0 || x_index >= MAP_SIZE || y_index < 0 ||
+          y_index >= MAP_SIZE)
+        continue;
+      char c = map[y_index][x_index];
+      if (c == '*') {
+        gear_x = x_index;
+        gear_y = y_index;
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void calculate_gear_ratio() {
+  int total = 0;
+  for (int i = 0; i <= gears_count; i++) {
+    if (gears[i].is_valid && gears[i].gears == 2) {
+      total += gears[i].parts[0] * gears[i].parts[1];
+    }
+  }
+  printf("Total:%d", total);
+}
+
+void check_map() {
+  for (int y = 0; y < MAP_SIZE; y++) {
+    char *pos = map[y];
+    char *lastPos = map[y + 1];
+    while (true) {
+      char *first_digit = strpbrk(pos, "0123456789");
+      if (first_digit >= map[y + 1] || first_digit == NULL) {
+        break;
+      }
+
+      char *last_digit = first_digit + strspn(first_digit, "0123456789");
+      int length = 0;
+      if (last_digit >= map[y + 1] || last_digit == NULL) {
+        last_digit = map[y + 1] - 1;
+        length = last_digit - first_digit;
+        length++;
+      } else {
+        length = last_digit - first_digit;
+      }
+
+      char number[10] = {0};
+      strncpy(number, first_digit, length);
+      int n = atoi(number);
+      pos = last_digit;
+
+      if (n == 0)
+        continue;
+      int x = first_digit - map[y];
+      bool is_touching = false;
+
+      for (int posX = x; posX < x + length; posX++) {
+        is_touching = is_touching_gear(posX, y);
+        if (is_touching) {
+          check_existing_gear_info(n);
         }
+        if (is_touching)
+          break;
+      }
+      if (last_digit >= lastPos - 1) {
+        break;
+      }
     }
-    AddToNewGear(n);
+  }
+  calculate_gear_ratio();
 }
 
-bool IsTouchingGear(int x, int y)
-{
-    for (int xIndex = x - 1; xIndex <= x + 1; xIndex++) {
-        for (int yIndex = y - 1; yIndex <= y + 1; yIndex++) {
-            if (xIndex == x && yIndex == y) continue;
-            if (xIndex < 0 || xIndex >= MAP_SIZE || yIndex < 0 || yIndex >= MAP_SIZE) continue;
-            char c = map[yIndex][xIndex];
-            if (c == '*') {
-                gearX = xIndex;
-                gearY = yIndex;
-                return true;
-            }
-        }
-    }
-    return false;
-}
+int main() {
+  FILE *input = fopen("../input.txt", "r");
 
-void CalculateGearRatio()
-{
-    int total = 0;
-    for (int i = 0; i <= gearsCount; i++) {
-        if (gears[i].isValid && gears[i].gears == 2) {
-            total += gears[i].parts[0] * gears[i].parts[1];
-            printf("%d * %d = %d\n", gears[i].parts[0], gears[i].parts[1], gears[i].parts[0] * gears[i].parts[1]);
-        }
-    }
-}
+  char line[256];
 
-void CheckMap()
-{
-    int total = 0;
-    for (int y = 0; y < MAP_SIZE; y++)
-    {
-        char* pos = map[y];
-        char* lastPos = map[y+1];
-        while (true)
-        {
-            char* firstDigit = strpbrk(pos, "0123456789");
-            if (firstDigit >= map[y + 1] || firstDigit == NULL)
-            {
-                break;
-            }
+  int y = 0;
+  for (int i = 0; fgets(line, 256, input) != NULL; i++) {
+    memcpy(&map[i], line, MAP_SIZE);
+  }
 
-            char* lastDigit = firstDigit + strspn(firstDigit, "0123456789");
-            int length = 0;
-            if (lastDigit >= map[y + 1] || lastDigit == NULL)
-            {
-                lastDigit = map[y + 1] - 1;
-                length = lastDigit - firstDigit;
-                length++;
-            }
-            else
-            {
-                length = lastDigit - firstDigit;
-            }
-
-            char number[10] = {0};
-            strncpy(number, firstDigit, length);
-            int n = atoi(number);
-            pos = lastDigit;
-
-            if (n == 0) continue;
-            int x = firstDigit - map[y];
-            bool isTouchingGear = false;
-
-            for (int posX = x; posX < x + length; posX++) {
-                isTouchingGear = IsTouchingGear(posX, y);
-                if (isTouchingGear)
-                {
-                    CheckExistingGearInfo(n);
-                }
-                if (isTouchingGear) break;
-            }
-            if (lastDigit >= lastPos - 1)
-            {
-                break;
-            }
-        }
-    }
-    CalculateGearRatio();
-}
-
-int main()
-{
-    FILE* input = fopen("../input.txt", "r");
-
-    char line[256];
-    
-    int y = 0;
-    for (int i = 0; fgets(line, 256, input) != NULL; i++) {
-        memcpy(&map[i], line, MAP_SIZE);
-    }
-    
-    for (int i = 0; i < MAX; i++) {
-        gears[i].x = 0;
-        gears[i].y = 0;
-        gears[i].parts[0] = 0;
-        gears[i].parts[1] = 0;
-        gears[i].gears = 0;
-        gears[i].isValid = false;
-    }
-    CheckMap();
+  for (int i = 0; i < MAX; i++) {
+    gears[i].x = 0;
+    gears[i].y = 0;
+    gears[i].parts[0] = 0;
+    gears[i].parts[1] = 0;
+    gears[i].gears = 0;
+    gears[i].is_valid = false;
+  }
+  check_map();
 }
