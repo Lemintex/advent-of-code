765a85530e046822959ba0a01f33c25e00919b8f
Lemintex <aaron_nobile@hotmail.co.uk>
2024-12-01 19:49:16 +0000

    Added solution for day 1

--- 2024/day-1/solution.go
+++ 2024/day-1/solution.go
@@ -3,12 +3,19 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"sort"
+	"strconv"
 	"strings"
+	"time"
 )
 
 var input []string
 var bigboy []string
+
+var l, r []int
+var bl, br []int
 
 // only for getting the input into []string format
 // put input parsing code in ParseInput()
@@ -37,25 +44,118 @@ func ReadBigBoy() {
 }
 
 func main() {
-	p1, bb1 := Part1()
-	p2, bb2 := Part2()
-	fmt.Println("Part 1:", p1)
+	Read()
+	Parse()
+	aoc1, bb1 := Part1()
+	aoc2, bb2 := Part2()
+	fmt.Println("==============================================")
+	fmt.Println("Part 1:", aoc1)
 	fmt.Println("Big Boy Part 1:", bb1)
-	fmt.Println("Part 2:", p2)
+	fmt.Println("Part 2:", aoc2)
 	fmt.Println("Big Boy Part 2:", bb2)
 }
 
+func Read() {
+	ReadInput()
+	ReadBigBoy()
+}
+
+func Parse() {
+	ParseInput()
+	ParseBigBoy()
+}
 // do all the stuff related to handling the input here
+
 func ParseInput() {
-
+	for _, s := range input {
+		leftID, rightID, _ := strings.Cut(s, "   ")
+		left, err := strconv.Atoi(leftID)
+		if err != nil {
+			return
+		}
+		right, err := strconv.Atoi(rightID)
+		if err != nil {
+			return
+		}
+		l = append(l, left)
+		r = append(r, right)
+	}
 }
 
-func Part1() (int, int) {
-	part1, bigboy := 0, 0
-	return part1, bigboy
+func ParseBigBoy() {
+	for _, s := range bigboy {
+		leftID, rightID, _ := strings.Cut(s, "   ")
+		left, err := strconv.Atoi(leftID)
+		if err != nil {
+			return
+		}
+		right, err := strconv.Atoi(rightID)
+		if err != nil {
+			return
+		}
+		bl = append(bl, left)
+		br = append(br, right)
+	}
 }
 
-func Part2() (int, int) {
-	part2, bigboy := 0, 0
-	return part2, bigboy
+func Part1() (int, int64) {
+	return Part1AOC(), Part1BigBoy()
+}
+
+func Part1AOC() int {
+	start := time.Now()
+	ans := 0
+	sort.Ints(l)
+	sort.Ints(r)
+	for i := range l {
+		dist := float64(l[i]-r[i])
+		ans += int(math.Abs(dist))
+	}
+	fmt.Println("AOC Part 1:", time.Since(start))
+	return ans
+}
+
+func Part1BigBoy() int64 {
+	start := time.Now()
+	ans := int64(0)
+	sort.Ints(bl)
+	sort.Ints(br)
+	for i := range bl {
+		dist := float64(bl[i]-br[i])
+		ans += int64(math.Abs(dist))
+	}
+	fmt.Println("Big Boy Part 1:", time.Since(start))
+	return ans
+}
+
+func Part2() (int, int64) {
+	return Part2AOC(), Part2BigBoy()
+}
+
+func Part2AOC() int {
+	start := time.Now()
+	ans := 0
+	occurrences := make(map[int]int)
+	for _, n := range r {
+		occurrences[n]++
+	}
+	for _, n := range l {
+		ans += n * occurrences[n]
+	}
+	fmt.Println("AOC Part 2:", time.Since(start))
+	return ans
+}
+
+func Part2BigBoy() int64 {
+	start := time.Now()
+	ans := int64(0)
+	occurrences := make(map[int]int)
+	for _, n := range br {
+		occurrences[n]++
+	}
+	for _, n := range bl {
+		ans += int64(n * occurrences[n])
+	}
+	fmt.Println("Big Boy Part 2:", time.Since(start))
+	return ans
 }
