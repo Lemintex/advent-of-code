0ab1d64417ba78e6ac91c32805ebcd90e5c72f59
Lemintex <aaron_nobile@hotmail.co.uk>
2024-12-02 22:29:01 +0000

    added messy solution for day 2

--- 2024/day-02/solution.go
+++ 2024/day-02/solution.go
@@ -3,13 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
 var input []string
 var bigboy []string
+
+var reportsIn [][]int
 
 // only for getting the input into []string format
 // put input parsing code in ParseInput()
@@ -58,10 +62,23 @@ func Parse() {
 	ParseInput()
 	ParseBigBoy()
 }
+
 // do all the stuff related to handling the input here
 
 func ParseInput() {
-
+	for _, i := range input {
+		strReport := strings.Split(i, " ")
+		var report []int
+		for _, l := range strReport {
+			level, err := strconv.Atoi(l)
+			if err != nil {
+				log.Fatal(err)
+				return
+			}
+			report = append(report, level)
+		}
+		reportsIn = append(reportsIn, report)
+	}
 }
 
 func ParseBigBoy() {
@@ -76,6 +93,11 @@ func Part1AOC() int {
 	start := time.Now()
 	ans := 0
 
+	for _, r := range reportsIn {
+		if isReportSafe(r) {
+			ans++
+		}
+	}
 	fmt.Println("AOC Part 1:", time.Since(start))
 	return ans
 }
@@ -94,8 +116,23 @@ func Part2() (int, int64) {
 
 func Part2AOC() int {
 	start := time.Now()
+	fmt.Println("TEST")
 	ans := 0
-
+	for _, r := range reportsIn {
+		for i := range r {
+			left := make([]int, i)
+			right := make([]int, len(r)-i-1)
+			copy(left, r[:i])
+			copy(right, r[i+1:])			
+			fmt.Println(left, right)
+			arr := append(left, right...)
+			fmt.Println(arr)
+			if isReportSafe(arr) {
+				ans++
+				break
+			}
+		}
+	}
 	fmt.Println("AOC Part 2:", time.Since(start))
 	return ans
 }
@@ -106,4 +143,20 @@ func Part2BigBoy() int64 {
 
 	fmt.Println("Big Boy Part 2:", time.Since(start))
 	return ans
+}
+
+func isReportSafe(r []int) bool {
+	asc := r[1] > r[0]
+	for i := 0; i < len(r)-1; i++ {
+		diff := r[i+1] - r[i]
+		diffAbs := math.Abs(float64(r[i+1] - r[i]))
+		if diffAbs < 1 || diffAbs > 3 {
+			return false
+		}
+		if (asc && diff > 0) || (!asc && diff < 0) {
+			continue
+		}
+		return false
+	}
+	return true
 }
